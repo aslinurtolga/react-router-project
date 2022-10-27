f1cced5105f38a898925be134b7ed1f6e2419a14
AslinurTolga <aslinurtolga@gmail.com>
2022-10-27 15:16:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>home page</div>} />
+        {/* this is going to reflect whatevet we have in the URL
+        so again, we install the package, we grab three components browser router, root and root browser or connect to the browser. Root is going to be a parent for all our routes and then for every page we'll set up a root component with two props a path and then what we to render, we pass into the element
+        */}
+
+        <Route
+          path="testing"
+          element={
+            <div>
+              <h2>Testing</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
